9731776704b78a194c7a7c5318aca99ece605978
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-05-28 20:50:59 +0530

    Project setup completed

---
+++ demo.py
@@ -0,0 +1,13 @@
+# from src.pipline.training_pipeline import TrainPipeline
+
+# pipline = TrainPipeline()
+# pipline.run_pipeline()
+
+
+from src.logger import logging
+
+logging.debug("This is a debug message.")
+logging.info("This is an info message.")
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
+logging.critical("This is a critical message.")
